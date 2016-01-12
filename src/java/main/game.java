d6edad6419a4cb5e14b5034a8cfe33ffd603f8ef
Vladimir <Vladimir@mightybook>
2016-01-12 16:58:39 +0500

    создание проекта

---
+++ src/java/main/game.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016 Vladimir
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package main;
+
+import java.applet.Applet;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class game extends Applet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+
+    // TODO overwrite start(), stop() and destroy() methods
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 12.01.2016, 16:23:28
+    Author     : Vladimir
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <applet code="org.me.main.game" archive="game.jar" width="600" height="480"/>
+    </body>
+</html>
