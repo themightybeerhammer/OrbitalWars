b169f7fc9e1bf373de3157fffc4891cacff6502a
themightybeerhammer <>
2016-01-14 12:29:27 +0500

    работает таймер с тестовой задачей перемещения объектов по экрану туда ->>> отрисовка выведена в DrawPanel.java добавил интерфейсы листенеров на будущее

--- src/java/main/game.java
+++ src/java/main/game.java
@@ -14,61 +14,89 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package main;
 
 import java.applet.Applet;
 import java.awt.Graphics;
 import java.awt.Paint;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author Vladimir
  */
 
-
-public class game extends Applet {
-
-    public ArrayList<BaseClass> ALBaseClass;
-    /**
-     * Initialization method that will be called after the applet is loaded into
-     * the browser.
-     */
+public class game extends Applet implements ActionListener, KeyListener {
+    
+    private ArrayList<BaseClass> ALBaseClass;
+    private DrawPanel Display;
+    
     public void init() {
         ALBaseClass = new ArrayList<>();
-        // TODO start asynchronous download of heavy resources
+        
+        new BaseClass(100, 90, 5, 5, 20, 20, ALBaseClass);
+        new BaseClass(22, 80, 5, 5, 20, 20, ALBaseClass);
+        new BaseClass(5, 110, 5, 5, 20, 20, ALBaseClass);       
+        new BaseClass(50, 50, 5, 5, 20, 20, ALBaseClass);
+        new BaseClass(77, 60, 5, 5, 20, 20, ALBaseClass);
+        new BaseClass(120, 100, 5, 5, 20, 20, ALBaseClass);
+        
+        Display = new DrawPanel();
+        add(Display);
+        
+        Timer oTimer = new Timer();
+        TimerTask oTimerTask = new TimerTask(){
+            @Override 
+            public void run(){  
+                for(int i = 0 ;i < ALBaseClass.size(); i++){
+                    ALBaseClass.get(i).X++;
+                }
+                Display.AssignList(ALBaseClass);
+            }
+        };
+        oTimer.schedule(oTimerTask, 0, 100);
     }
+    
+    public void CalcCenterMass(Graphics g){
+        int   Xc = ALBaseClass.get(0).X;
+        int   Yc = ALBaseClass.get(0).Y;
+        float Mc = ALBaseClass.get(0).M;
+        for(int i = 1; i < ALBaseClass.size(); i++){
+            Xc = Xc + (Xc - ALBaseClass.get(i).X) * (int)(ALBaseClass.get(i).M / Mc);
+            Yc = Yc + (Yc - ALBaseClass.get(i).Y) * (int)(ALBaseClass.get(i).M / Mc);
+            Mc = Mc + ALBaseClass.get(i).M;
+        }  
+    }
+    
     @Override
-    public void paint(Graphics g){
-        
-        new BaseClass(100,90,5,5,20,20,ALBaseClass);
-        new BaseClass(22,80,5,5,20,20,ALBaseClass);
-        new BaseClass(5,110,5,5,20,20,ALBaseClass);
-        
-        new BaseClass(50,50,5,5,20,20,ALBaseClass);
-        new BaseClass(77,60,5,5,20,20,ALBaseClass);
-        new BaseClass(120,100,5,5,20,20,ALBaseClass);
-              
-        for(int i=0;i<ALBaseClass.size();i++){
-          ALBaseClass.get(i).draw(g);
-        }
+    public void destroy(){
     }
     
-   public void CalcCenterMass(Graphics g){
-      
-       int   Xc=ALBaseClass.get(0).X;
-       int   Yc=ALBaseClass.get(0).Y;
-       float Mc=ALBaseClass.get(0).M; 
-       
-       for(int i=1;i<ALBaseClass.size();i++){
-          Xc = Xc+(Xc-ALBaseClass.get(i).X)* (int)(ALBaseClass.get(i).M/Mc);
-          Yc = Yc+(Yc-ALBaseClass.get(i).Y)* (int)(ALBaseClass.get(i).M/Mc);
-          Mc=Mc+ALBaseClass.get(i).M;
-        }
-       
-      
-       
-   } 
+    @Override 
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
+    @Override 
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
-    // TODO overwrite start(), stop() and destroy() methods
+    @Override 
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override 
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
